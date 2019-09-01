53749fc83c41fc659a27523d9733b9c6507672d5
Rebecca Valentine <psygnisfive@yahoo.com>
2019-08-31 17:10:56 -0700

    Splits by newlines.

--- shodan_say.py
+++ shodan_say.py
@@ -6,24 +6,30 @@ import random
 import string
 import sys
 import os
+import re
 from subprocess import call
 
 if len(sys.argv) != 2:
 
-  print('I need something to say!')
+    print('I need something to say!')
 
 else:
 
-  lang = 'en'
-  text = urllib.parse.quote(sys.argv[1])
-  url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl=%s&client=tw-ob&q=%s' % (lang, text)
-  output = 'files/' + ''.join(random.choice(string.hexdigits) for i in range(32)) + '.mp3'
-  r = requests.get(url)
+    lang = 'en'
 
-  if 200 == r.status_code:
-    if not os.path.isdir('files'):
-      os.makedirs('files')
-    with open(output, 'wb') as f:
-      f.write(r.content)
-    call('mpg123 %s' % output, shell=True)
-    os.remove(output)
+    lines = sys.argv[1].split('\\n')
+    print(lines)
+    for line in lines:
+        url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl=%s&client=tw-ob&q=%s' % (
+            lang, line)
+        output = 'files/' + ''.join(random.choice(string.hexdigits)
+                                    for i in range(32)) + '.mp3'
+        r = requests.get(url)
+
+        if 200 == r.status_code:
+            if not os.path.isdir('files'):
+                os.makedirs('files')
+            with open(output, 'wb') as f:
+                f.write(r.content)
+            call('mpg123 %s' % output, shell=True)
+            os.remove(output)
